f0f120e5c8de2f19b134c1a69f788cdb37abffe1
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-05-02 23:32:54 -0500

    Added tests for StudentController's getByCertification method

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -2,10 +2,20 @@ const StudentController = require("../../lib/controllers/StudentController");
 const StudentService = require("./../../lib/services/StudentService");
 const Reader = require("./../../lib/utils/Reader");
 
-describe('Name of the group', () => {
+describe('Tests for getAll', () => {
     test('should get all the elements in the list', () => {
         const students = Reader.readJsonFile("visualpartners.json");
         const result = StudentController.getAll();
         expect(result).toEqual(students);
     });
 });
+
+describe('Tests for getByCertification', () => {
+    test('should get the correct students', () => {
+        const students = Reader.readJsonFile("visualpartners.json");
+        const resultFromService = StudentService.filterByCertification(students, true);
+        const resultFromController = StudentController.getByCertification(true);
+        expect(resultFromController).toEqual(resultFromService);
+    });
+});
+
