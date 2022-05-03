5ab0423ea91b3bd67d53177b983e3be6b5f5e066
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-05-02 22:03:27 -0500

    Updated description of test group

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,6 +1,6 @@
 const StudentService = require("./../../lib/services/StudentService")
 const Reader = require("./../../lib/utils/Reader")
-describe('Tests for getAllStudents', () => {
+describe('Tests for getStudentsByCertification', () => {
     test('should get students by certification', () => {
         const students = [{id: "123a", haveCertification: true},{id: "456b", haveCertification:false},{id: "789c", haveCertification:true}]
         const studentsFiltered = StudentService.getStudentsByCertification(students, true);
