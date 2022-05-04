daee9a64ba87bb5cb69e45cbb3b2583ee1fb4a2b
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-05-04 15:54:01 -0500

    Added tests for StudentController's getByCredits method

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -19,3 +19,15 @@ describe('Tests for getByCertification', () => {
     });
 });
 
+describe('Tests for getByCredits', () => {
+    test('should get the correct students', () => {
+        const students = Reader.readJsonFile("visualpartners.json");
+        const resultFromService = StudentService.filterByCredits(students, "<=5", ["==", "<=", ">=", "<", ">"]);
+        const resultFromController = StudentController.getByCredits("<=5");
+        expect(resultFromController).toEqual(resultFromService);
+    });
+    test('should get error message if invalid expression', () => {
+        const result = StudentController.getByCredits("-4");
+        expect(result).toEqual({error: `invalid expression "-4"`, validExpressions: ["==", "<=", ">=", "<", ">"]});
+    });
+});
